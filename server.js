2199656be420279fdecfdd2e9f12f91ea045f042
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-05-17 01:01:47 +0530

    UPDATE: microservice endpoints updated

--- server.js
+++ server.js
@@ -13,18 +13,21 @@ app.use(express.urlencoded({ extended: false }));
 const { verifyAuth } = require("./middleware/auth.middleware");
 
 const PORT = process.env.PORT || 5000;
-const BASE_URL = "http://localhost:";
+
 const PRODUCT_URL = "http://34.136.127.97:5001";
 const USER_URL = "http://35.188.186.178:5002";
 const PAYMENT_URL = "http://20.24.130.1:8080";
 const ORDER_URL = "http://35.192.50.196:5000";
+const AUTH_URL = "http://20.212.122.130:5000";
+const FEEDBACK_URL = "";
+const ADMIN_URL = "http://35.192.106.101:5000";
 
 app.use("/products", verifyAuth, proxy(PRODUCT_URL));
 app.use("/users", verifyAuth, proxy(USER_URL));
 app.use("/orders", verifyAuth, proxy(ORDER_URL));
-app.use("/feedbacks", verifyAuth, proxy(`${BASE_URL}5004`));
-app.use("/admin", verifyAuth, proxy(`${BASE_URL}5005`));
-app.use("/auth", verifyAuth, proxy(`${BASE_URL}5006`));
+app.use("/feedbacks", verifyAuth, proxy(FEEDBACK_URL));
+app.use("/admin", verifyAuth, proxy(ADMIN_URL));
+app.use("/auth", verifyAuth, proxy(AUTH_URL));
 app.use("/payments", verifyAuth, proxy(PAYMENT_URL));
 
 app.get("/", (req, res) => res.status(200).send("<h3>SHOP API - GATEWAY SERVICE</h3>"));
